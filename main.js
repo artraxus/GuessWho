b22569cbbe004fe5cc820925d50bbabf8bf5e5d6
Clément Folliet <cfolliet@talentsoft.com>
2014-03-06 13:52:57 +0100

    little init of the project (add main.js and package.json)

---
+++ main.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello world');
+});
+
+app.listen(8080);
