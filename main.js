433214d1e133e6e436b7673276246fba3502865d
Clément Folliet <cfolliet@talentsoft.com>
2014-04-01 20:16:56 +0200

    add game routes and first try of socket io

--- main.js
+++ main.js
@@ -1,10 +1,56 @@
 var express = require('express');
+var http = require('http');
+var path = require('path');
+var uuid = require('node-uuid');
+var socketIo = require('socket.io');
+
+var __dirname = '.';
+var publicDirectory = 'public';
+
+var games = [];
+
+function Game() {
+    return {
+        id: uuid.v4(),
+        title: 'totor',
+        players: []
+    };
+};
 
 var app = express();
+app.use(express.static(path.join(__dirname, publicDirectory)));
 
-app.get('/', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello world');
+app.get('/', function (request, response) {
+    response.render('index.html');
 });
 
-app.listen(8080);
+app.get('/games', function (request, response) {
+    response.send(games);
+});
+
+app.get('/games/:id', function (request, response) {
+    var i = 0;
+    while (games[i].id != request.params.id) {
+        i++;
+    }
+    response.send(games[i]);
+});
+
+app.post('/games', function (request, response) {
+    var game = new Game();
+    games.push(game);
+    response.send(game);
+});
+
+//app.listen(8080);
+console.log('server is running...');
+
+var server = http.createServer(app);
+io = socketIo.listen(server);
+server.listen(8080);
+
+io.sockets.on('connection', function (socket) {
+    socket.on('subscribeMessage', function (data) {
+        io.sockets.emit('subscribeMessage', data);
+    });
+});
